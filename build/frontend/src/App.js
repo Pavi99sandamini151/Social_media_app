eecaf8f2aeae301b974732dcad605517ee93b504
Pavi99 <pavithra@smbapps.com.au>
2023-05-07 06:37:10 +0530

    added files

--- build/frontend/src/App.js
+++ build/frontend/src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+// Make a GET request to your Firebase Functions endpoint
+axios.get('https://us-central1-my-project.cloudfunctions.net/myFunction')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
   return (
     <div className="App">
       <header className="App-header">
--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,14 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+const cors = require('cors')({ origin: true });
+
+// Create an HTTP endpoint for your frontend
+exports.myFunction = functions.https.onRequest((request, response) => {
+  // Use the cors middleware to handle CORS
+  cors(request, response, () => {
+    // Handle your API endpoint logic here
+    response.send('Hello from Firebase Functions!');
+  });
+});
